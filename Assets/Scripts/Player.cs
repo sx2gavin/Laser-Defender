46ea7c0401f11f291025173e719e710ea94343ac
Gavin Luo <luoxiangyu528@gmail.com>
2018-11-17 23:05:29 -0500

    Add script to move the plane frame-independently.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var newXPos = transform.position.x + deltaX;
+
+		var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		var newYPos = transform.position.y + deltaY;
+		transform.position = new Vector3(newXPos, newYPos, transform.position.z);
+	}
+}
