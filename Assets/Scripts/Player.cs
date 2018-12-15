43eb9e1ffd47d41c37b91d66725a785bb48a112b
Gavin Luo <luoxiangyu528@gmail.com>
2018-12-14 23:35:04 -0500

    Add coroutines to fire continuously.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,27 +5,76 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	// configuration parameters
 	[SerializeField] float speed;
+	[SerializeField] GameObject laserPrefab;
+	[SerializeField] float projectileSpeed = 10f;
+	[SerializeField] float projectileFiringPeriod = 0.1f;
+
+	Coroutine firingCoroutine;
+
+	float xMin;
+	float xMax;
+	float yMin;
+	float yMax;
+
+	SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		SetUpMoveBoundaries();
 	}
-	
+
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
 		Move();
+		Fire();
+	}
+
+	private void Fire()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			firingCoroutine = StartCoroutine(FireContinuously());
+		}
+		else if (Input.GetButtonUp("Fire1"))
+		{
+			StopCoroutine(firingCoroutine);
+		}
+	}
+
+	private IEnumerator FireContinuously()
+	{
+		while (true)
+		{
+			GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+			laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+			yield return new WaitForSeconds(projectileFiringPeriod);
+		}
+	}
+
+	private void SetUpMoveBoundaries()
+	{
+		float halfWidthOfSprite = spriteRenderer.size.x / 2;
+		float halfHeightOfSprite = spriteRenderer.size.y / 2;
+		Camera gameCamera = Camera.main;
+		xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + halfWidthOfSprite;
+		xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - halfWidthOfSprite;
+		yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + halfHeightOfSprite;
+		yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - halfHeightOfSprite;
 	}
 
 	private void Move()
 	{
 		var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-		var newXPos = transform.position.x + deltaX;
-
 		var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-		var newYPos = transform.position.y + deltaY;
+
+		var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+		var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
 		transform.position = new Vector3(newXPos, newYPos, transform.position.z);
 	}
 }
